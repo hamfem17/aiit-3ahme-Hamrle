1882e1e0e4d54226f4f75ef9a49c466a82657248
hamfem17 <57904984+hamfem17@users.noreply.github.com>
2020-03-30 14:10:47 +0200

    Create Taschenrechner.java

---
+++ corona-java-Hamrle/ue01_taschenrechner/Taschenrechner.java
@@ -0,0 +1,28 @@
+package ue01_taschenrechner;
+
+public class Taschenrechner
+{
+	
+	public static double a;
+	public static double b;
+	
+	public static double getAdditionResult(double a, double b)
+	{
+		return a + b;
+	}
+	
+	public static double getSubtractionResult(double a, double b)
+	{
+		return a - b;
+	}
+
+	public static double getMultiplicationResult(double a, double b)
+	{
+		return a * b;
+	}
+
+	public static double getDivisionResult(double a, double b)
+	{
+		return a / b;
+	}
+}
