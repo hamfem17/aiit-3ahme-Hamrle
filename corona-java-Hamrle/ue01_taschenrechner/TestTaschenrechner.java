712a7c749d4ec4c042c6e88feaba5752218ed68f
hamfem17 <57904984+hamfem17@users.noreply.github.com>
2020-03-30 14:34:49 +0200

    Update TestTaschenrechner.java

--- corona-java-Hamrle/ue01_taschenrechner/TestTaschenrechner.java
+++ corona-java-Hamrle/ue01_taschenrechner/TestTaschenrechner.java
@@ -6,22 +6,22 @@ public class TestTaschenrechner
 {
 	public static void main(String[] args)
 	{
-	Scanner reader = new Scanner(System.in);
-	System.out.print("Wert a:");
-	Taschenrechner.a = reader.nextDouble();
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Wert a:");
+		Taschenrechner.a = reader.nextDouble();
 		
-	System.out.print("Wert b:");
-	Taschenrechner.b = reader.nextDouble();
-	reader.close();
+		System.out.print("Wert b:");
+		Taschenrechner.b = reader.nextDouble();
+		reader.close();
 		
-	String addition = String.format("%.3f", Taschenrechner.getAdditionResult(Taschenrechner.a, Taschenrechner.b));
-        String subtraction = String.format("%.3f", Taschenrechner.getSubtractionResult(Taschenrechner.a, Taschenrechner.b));
-        String multiplication = String.format("%.3f", Taschenrechner.getMultiplicationResult(Taschenrechner.a, Taschenrechner.b));
-        String division = String.format("%.3f", Taschenrechner.getDivisionResult(Taschenrechner.a, Taschenrechner.b));
+		String addition = String.format("%.3f", Taschenrechner.getAdditionResult(Taschenrechner.a, Taschenrechner.b));
+        	String subtraction = String.format("%.3f", Taschenrechner.getSubtractionResult(Taschenrechner.a, Taschenrechner.b));
+        	String multiplication = String.format("%.3f", Taschenrechner.getMultiplicationResult(Taschenrechner.a, Taschenrechner.b));
+        	String division = String.format("%.3f", Taschenrechner.getDivisionResult(Taschenrechner.a, Taschenrechner.b));
 		
-	System.out.println(Taschenrechner.getAdditionResult(Taschenrechner.a, Taschenrechner.b));
-	System.out.println(Taschenrechner.getSubtractionResult(Taschenrechner.a, Taschenrechner.b));
-	System.out.println(Taschenrechner.getMultiplicationResult(Taschenrechner.a, Taschenrechner.b));
-	System.out.println(Taschenrechner.getDivisionResult(Taschenrechner.a, Taschenrechner.b));
+		System.out.println(Taschenrechner.getAdditionResult(Taschenrechner.a, Taschenrechner.b));
+		System.out.println(Taschenrechner.getSubtractionResult(Taschenrechner.a, Taschenrechner.b));
+		System.out.println(Taschenrechner.getMultiplicationResult(Taschenrechner.a, Taschenrechner.b));
+		System.out.println(Taschenrechner.getDivisionResult(Taschenrechner.a, Taschenrechner.b));
 	}
 }
